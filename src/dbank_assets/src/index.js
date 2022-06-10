aede59b2d552fe5a3196eadb97a5d87ae841492d
Athika Fatima <athikafatima16@gmail.com>
2022-06-10 23:49:58 +0530

    Completed project dbank with UI

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,43 @@
 import { dbank } from "../../declarations/dbank";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
-
-  const name = document.getElementById("name").value.toString();
-
-  button.setAttribute("disabled", true);
-
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
-
-  button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+window.addEventListener("load", async function () {
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerHTML =
+    Math.round(currentAmount * 100) / 100;
 });
+
+document
+  .querySelector("form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const button = event.target.querySelector("#submit-btn");
+
+    const inputAmount = parseFloat(
+      document.getElementById("input-amount").value
+    );
+    const OutputAmount = parseFloat(
+      document.getElementById("withdraw-amount").value
+    );
+
+    button.setAttribute("disabled", true);
+
+    if (document.getElementById("input-amount").value.length != 0) {
+      await dbank.topUp(inputAmount);
+    }
+
+    if (document.getElementById("withdraw-amount").value.length != 0) {
+      await dbank.withdraw(OutputAmount);
+    }
+
+    await dbank.compound();
+
+    const currentAmount = await dbank.checkBalance();
+    document.getElementById("value").innerHTML =
+      Math.round(currentAmount * 100) / 100;
+
+    document.getElementById("input-amount").value = "";
+    document.getElementById("withdraw-amount").value = "";
+
+    button.removeAttribute("disabled");
+  });
